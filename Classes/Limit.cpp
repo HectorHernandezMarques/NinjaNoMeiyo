2714d5358a9d5ad524a32d2c726c4e8b865237ee
Héctor Hernández Marqués <H.Hernandez@alumnos.upm.es>
2017-06-07 01:13:40 +0200

    LIMITS DIAGONAL

--- Classes/Limit.cpp
+++ Classes/Limit.cpp
@@ -9,10 +9,11 @@ NinjaM::Limit::Limit()
 
 }
 
-NinjaM::Limit::Limit(cocos2d::Size visibleSize, cocos2d::Vec2 position, cocos2d::Vec2 anchorPoint, std::string texture, float rotation, cocos2d::Size size) : PhysicNode(visibleSize, position, anchorPoint, texture, rotation)
+NinjaM::Limit::Limit(cocos2d::Size visibleSize, cocos2d::Vec2 position, cocos2d::Vec2 anchorPoint, std::string texture, float rotation, cocos2d::Size contentSize, int count) : PhysicNode(visibleSize, position, anchorPoint, texture, rotation)
 {
     CCLOG("Limit Builder");
-	contentSize = size;
+	this->contentSize = contentSize;
+	this->count = count;
 }
 
 NinjaM::Limit::~Limit()
@@ -20,13 +21,24 @@ NinjaM::Limit::~Limit()
 
 }
 
+void NinjaM::Limit::setPoints(cocos2d::Vec2 p0, cocos2d::Vec2 p1, cocos2d::Vec2 p2, cocos2d::Vec2 p3)
+{
+	CCLOG("%f", p0.x);
+	this->points[0] = p0;
+	this->points[1] = p1;
+	this->points[2] = p2;
+	this->points[3] = p3;
+}
+
 void NinjaM::Limit::spawn(cocos2d::Layer *layer, int limitBitmask)
 {
 	this->nodeSprite = cocos2d::Sprite::create();
-	this->nodeSprite->setPosition(cocos2d::Vec2(this->position.x + this->contentSize.width/2, this->position.y + this->contentSize.height/2));
+	this->nodeSprite->setPosition(cocos2d::Vec2(this->position.x, this->position.y));
+	//this->nodeSprite->setPosition(cocos2d::Vec2(this->position.x + this->contentSize.width/2, this->position.y + this->contentSize.height/2));
 	this->nodeSprite->setAnchorPoint(this->anchorPoint);
 
-	this->nodeBody = cocos2d::PhysicsBody::createBox(this->contentSize);
+	this->nodeBody = cocos2d::PhysicsBody::createPolygon(this->points, this->count);
+	//this->nodeBody = cocos2d::PhysicsBody::createBox(this->contentSize);
 	this->nodeBody->setCollisionBitmask(FIXED_OBSTACLE_BITMASK);
 	this->nodeBody->setDynamic(false);
 	this->nodeBody->setContactTestBitmask(true);
--- Classes/Limit.h
+++ Classes/Limit.h
@@ -16,14 +16,16 @@ namespace NinjaM{
     {
     public:
         Limit();
-        Limit(cocos2d::Size visibleSize, cocos2d::Vec2 position, cocos2d::Vec2 anchorPoint, std::string texture, float rotation, cocos2d::Size size);
+        Limit(cocos2d::Size visibleSize, cocos2d::Vec2 position, cocos2d::Vec2 anchorPoint, std::string texture, float rotation, cocos2d::Size contentSize, int count);
         virtual ~Limit();
 
-
+		void setPoints(cocos2d::Vec2 p0, cocos2d::Vec2 p1, cocos2d::Vec2 p2, cocos2d::Vec2 p3);
         virtual void spawn(cocos2d::Layer *layer, int limitBitmask);
 
     private:
 		cocos2d::Size contentSize;
+		cocos2d::Vec2 points[4];
+		int count;
     };
 }
 
